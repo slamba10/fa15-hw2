30c9f58144008b92c74ca960e1ba5992529d11b5
brownboy69 <slamba10@gmail.com>
2015-09-25 20:51:18 -0700

    submitting hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,11 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  def initialize(baz)
+  	@baz = baz
+  end
+
+  def bar(cat, optional={})
+  	return "#{cat}#{@baz}#{optional[:sat]}" 
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,32 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
-    @text = "You are nothing!"
+    # if :name.blank? and :adjective.blank?
+    #   @text = "You are nothing!"
+    # elsif :name.blank?
+    #   @text = "You are " + :adjective + "!"
+    # elsif :adjective.blank?
+    #   @text = :name + " is nothing!"
+    # else
+    #   @text = :name + " is " + :adjective + "!"
+    @name = params[:name]
+    @adjective = params[:adjective]
+    # @text = :name 
   end
 
   def age
   end
 
   def person
+    @name = params[:name]
+    @age = params[:age]
+    person = Person.new @name, @age
+    @intro = person.introduce
+    @yob = person.birth_year
+    @nickname = person.nickname
   end
 end
